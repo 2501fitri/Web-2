2fbcd3802075ec9d387e84f9d45a6173760a91d9
SaimaFitri <sp797615@gmail.com>
2025-03-12 21:04:50 +0700

    praktikum03

---
+++ Praktikumtugas/partials/cards/project-card-1.php
@@ -0,0 +1,11 @@
+<div class="card overflow-hidden shadow rounded-4 border-0 mb-5">
+                                <div class="card-body p-0">
+                                    <div class="d-flex align-items-center">
+                                        <div class="p-5">
+                                            <h2 class="fw-bolder">Alexander Phlie</h2>
+                                            <p>Gambar seseorang tersenyum mengenakan kemeja hitam.Dikenakan kacamata dan memiliki janggut serta kumis.Pose menunjukkan tangan disilangkan.</p>
+                                        </div>
+                                        <img class="profile-img" src="assets/profile.png" alt="..." />
+                                    </div>
+                                </div>
+</div>
